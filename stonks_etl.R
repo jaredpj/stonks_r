123fe498745633dba5a33f5bf9b36d17d0e55c47
Jared Holder <jaredholder@jareds-mbp.lan>
2022-04-13 21:26:28 -0500

    update to etl script

--- stonks_etl.R
+++ stonks_etl.R
@@ -9,16 +9,41 @@ library(alphavantager)
 # Define tickers
 tickers<-c('DIS', 'MSFT', 'WFC', 'V', 'AAPL'
            , 'SQ', 'XLY', 'XLK', 'INTC', 'MRNA'
-)
+           )
 
 # API Key
 av_api_key('JFWC5K8EAVN6SWO6')
 
-# 
-df<-av_get(symbol = "MSFT"
-           , av_fun = "TIME_SERIES_INTRADAY"
-           , interval = "1min"
-           , outputsize = "full"
-)
+# Declare data frame list
+df_list<-list()
 
-for i in 
+# Loop through tickers (could also write custom function and do `apply()`)
+for (i in 1:length(tickers)){
+  
+  # Get data for ticker i
+  df<-av_get(symbol = tickers[i]
+             , av_fun = "TIME_SERIES_INTRADAY"
+             , interval = "1min"
+             , outputsize = "full"
+             )
+  
+  # Add ticker symbol as an index column
+  df$ticker<-c(tickers[i])
+  
+  # Put data frame into list element
+  df_list[[i]]<-df
+  
+  # Take a 30 second break so the API doesn't complain about too many hits
+  Sys.sleep(30)
+
+  remove(df) # we won't need this df anymore
+  gc() # go ahead and do a garbage clean up to clear the RAM
+
+}
+
+# Bind data frame list into single data frame
+df<-bind_rows(df_list)
+
+# Export to .rdata (more memory efficient for production)
+save(df, file = 'stonks.rdata')
+
